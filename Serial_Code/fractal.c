d46e13e8a8c940791ed82ed0bdc5b3499b91af96
AdhamNiazi <adham.k.niazi@gmail.com>
2023-05-31 00:08:34 -0700

    Added WASD movement to recalculatae Mebdelbrot

--- Serial_Code/fractal.c
+++ Serial_Code/fractal.c
@@ -1,115 +1,117 @@
-/*
-fractal.c - Sample Mandelbrot Fractal Display
-Starting code for CSE 30341 Project 3.
-*/
-
 #include "gfx.h"
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
 #include <errno.h>
 #include <string.h>
 #include <complex.h>
+#include <omp.h>
 
-/*
-Compute the number of iterations at point x, y
-in the complex space, up to a maximum of maxiter.
-Return the number of iterations at that point.
 
-This example computes the Mandelbrot fractal:
-z = z^2 + alpha
+// Variables to store the current position
+double xmin = -1.5;
+double xmax = 0.5;
+double ymin = -1.0;
+double ymax = 1.0;
 
-Where z is initially zero, and alpha is the location x + iy
-in the complex plane.  Note that we are using the "complex"
-numeric type in C, which has the special functions cabs()
-and cpow() to compute the absolute values and powers of
-complex values.
-*/
 
-static int compute_point( double x, double y, int max )
+
+
+static int compute_point(double x, double y, int max)
 {
-	double complex z = 0;
-	double complex alpha = x + I*y;
+    double complex z = 0;
+    double complex alpha = x + I * y;
 
-	int iter = 0;
+    int iter = 0;
 
-	while( cabs(z)<4 && iter < max ) {
-		z = cpow(z,2) + alpha;
-		iter++;
-	}
+    while (cabs(z) < 4 && iter < max)
+    {
+        z = cpow(z, 2) + alpha;
+        iter++;
+    }
 
-	return iter;
+    return iter;
 }
 
-/*
-Compute an entire image, writing each point to the given bitmap.
-Scale the image to the range (xmin-xmax,ymin-ymax).
-*/
-
-void compute_image( double xmin, double xmax, double ymin, double ymax, int maxiter )
+// Function to compute the fractal image
+void compute_image(double xmin, double xmax, double ymin, double ymax, int maxiter)
 {
-	int i,j;
+    int i, j;
 
-	int width = gfx_xsize();
-	int height = gfx_ysize();
+    int width = gfx_xsize();
+    int height = gfx_ysize();
 
-	// For every pixel i,j, in the image...
+  //  #pragma omp parallel for private(i) shared(width, height, xmin, xmax, ymin, ymax, maxiter)
+    for (j = 0; j < height; j++)
+    {
+        for (i = 0; i < width; i++)
+        {
+            double x = xmin + i * (xmax - xmin) / width;
+            double y = ymin + j * (ymax - ymin) / height;
 
-	for(j=0;j<height;j++) {
-		for(i=0;i<width;i++) {
-
-			// Scale from pixels i,j to coordinates x,y
-			double x = xmin + i*(xmax-xmin)/width;
-			double y = ymin + j*(ymax-ymin)/height;
-
-			// Compute the iterations at x,y
-			int iter = compute_point(x,y,maxiter);
-
-			// Convert a iteration number to an RGB color.
-			// (Change this bit to get more interesting colors.)
-			int gray = 255 * iter / maxiter;
-			gfx_color(gray,gray,gray);
-
-			// Plot the point on the screen.
-			gfx_point(i,j);
-		}
-	}
+            int iter = compute_point(x, y, maxiter);
+            int gray = 255 * iter / maxiter;
+            gfx_color(gray, gray, gray);
+            gfx_point(i, j);
+        }
+    }
 }
 
-int main( int argc, char *argv[] )
+// Function to update the position based on the key input
+void update_position(char key, double *xmin, double *xmax, double *ymin, double *ymax)
 {
-	// The initial boundaries of the fractal image in x,y space.
+    double xstep = (*xmax - *xmin) / 10;
+    double ystep = (*ymax - *ymin) / 10;
 
-	double xmin=-1.5;
-	double xmax= 0.5;
-	double ymin=-1.0;
-	double ymax= 1.0;
+    switch (key)
+    {
+    case 'w':
+        *ymin -= ystep;
+        *ymax -= ystep;
+        break;
+    case 'a':
+        *xmin -= xstep;
+        *xmax -= xstep;
+        break;
+    case 's':
+        *ymin += ystep;
+        *ymax += ystep;
+        break;
+    case 'd':
+        *xmin += xstep;
+        *xmax += xstep;
+        break;
+    }
+}
 
-	// Maximum number of iterations to compute.
-	// Higher values take longer but have more detail.
-	int maxiter=500;
+int main(int argc, char *argv[])
+{
+    int maxiter = 500;
+ 
 
-	// Open a new window.
-	gfx_open(640,480,"Mandelbrot Fractal");
+    gfx_open(640, 480, "Mandelbrot Fractal");
 
-	// Show the configuration, just in case you want to recreate it.
-	printf("coordinates: %lf %lf %lf %lf\n",xmin,xmax,ymin,ymax);
+    printf("coordinates: %lf %lf %lf %lf\n", xmin, xmax, ymin, ymax);
 
-	// Fill it with a dark blue initially.
-	gfx_clear_color(0,0,255);
-	gfx_clear();
+    gfx_clear_color(0, 0, 255);
+    gfx_clear();
 
-	// Display the fractal image
-	compute_image(xmin,xmax,ymin,ymax,maxiter);
+    compute_image(xmin, xmax, ymin, ymax, maxiter);
 
-	while(1) {
-		// Wait for a key or mouse click.
-		int c = gfx_wait();
+    while (1)
+    {
+        int c = gfx_wait();
 
-		// Quit if q is pressed.
-		if(c=='q') exit(0);
-	}
+        if (c == 'q')
+            exit(0);
+        else if (c == 'w' || c == 'a' || c == 's' || c == 'd')
+        {
+            update_position(c, &xmin, &xmax, &ymin, &ymax);
+        }
 
-	return 0;
+        gfx_clear();
+        compute_image(xmin, xmax, ymin, ymax, maxiter);
+    }
+
+    return 0;
 }
